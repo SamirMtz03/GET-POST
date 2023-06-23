de35819261fac99c6cfa32dd51934657abdb2f98
SamirMtz03 <136374607+SamirMtz03@users.noreply.github.com>
2023-06-23 08:28:21 -0600

    Version 1
    Esta es la primer subida de archivos

---
+++ datos.php
@@ -0,0 +1,7 @@
+<?php
+//$nombre = $_GET ['txtNombre'];
+$nombre = $_POST ['txtNombre'];
+//$apellido = $_GET ['txtApellido'];
+$apellido = $_POST ['txtApellido'];
+echo $nombre. "/" . $apellido;
+?>
